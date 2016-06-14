637f690cc2049b62f07c5c800d36ab29a1367866
Sean McGivern <sean@gitlab.com>
2016-06-14 14:55:30 +0100

    Allow modes to be lazily loaded
    The modes files are 6 MB of JavaScript, so requiring them all in the
    main application.js isn't practical for sites that want to support all
    modes.

--- lib/ace/rails/engine.rb
+++ lib/ace/rails/engine.rb
@@ -2,7 +2,7 @@ module Ace
   module Rails
     class Engine < ::Rails::Engine
       initializer 'ace-rails-ap.assets.precompile' do |app|
-        app.config.assets.precompile += %w[ace/worker-*.js]
+        app.config.assets.precompile += %w[ace/worker-*.js ace/mode-*.js]
       end
     end
   end
