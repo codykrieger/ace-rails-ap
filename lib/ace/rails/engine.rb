40b34ffd05d263e0cfae3954977f58d2962b219c
Ben Polinsky <benjamin.polinsky@gmail.com>
2016-06-25 01:05:21 -0400

    change snippet path

--- lib/ace/rails/engine.rb
+++ lib/ace/rails/engine.rb
@@ -2,7 +2,7 @@ module Ace
   module Rails
     class Engine < ::Rails::Engine
       initializer 'ace-rails-ap.assets.precompile' do |app|
-        app.config.assets.precompile += %w[ace/worker-*.js]
+        app.config.assets.precompile += %w[ace/worker-*.js ace/snippets/*.js]
       end
     end
   end
