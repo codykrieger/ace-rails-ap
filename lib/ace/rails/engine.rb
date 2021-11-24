3f846ef8ef57f327536ccef482f9b3fab67cccbe
Jan-Willem van der Meer <mail@jewilmeer.nl>
2021-11-24 13:57:28 +0100

    Allow lazy loading extensions
    This should fix the search extension. With the following error:
    
    ```
    Failed to load resource: the server responded with a status of 404 (Not Found) on /assets/ace/ext-searchbox.js
    ```
    
    In order to make it work the search module should be loaded with the
    proper asset_path reference, just like the workers and modes.

--- lib/ace/rails/engine.rb
+++ lib/ace/rails/engine.rb
@@ -2,9 +2,8 @@ module Ace
   module Rails
     class Engine < ::Rails::Engine
       initializer 'ace-rails-ap.assets.precompile' do |app|
-        app.config.assets.precompile += %w[ace/worker-*.js ace/mode-*.js]
+        app.config.assets.precompile += %w[ace/worker-*.js ace/mode-*.js ace/ext-*.js]
       end
     end
   end
 end
-
